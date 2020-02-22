d10055ca97e6113151aaf1ec6a03c2e23a431432
Ira <ira.d.sanchez@gmail.com>
2020-02-21 19:58:07 -0700

    And Get to RIngerBuffer - Still failing test due to unfinished append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,30 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # append changes depending on if the list is full or not
+
+        if self.capacity == self.storage.length:
+            # override the oldest element (head) with the item
+            # self.storage.head
+            print("at capacity")
+            # i think i need get() for this to work so that I can replace items in the middle
+        else:
+            self.storage.add_to_tail(item)
+            print("\n\nstorage: ", self.storage.length, "\n\n")
+            print(self.get())
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        current = self.storage.head
 
+        while current is not None:
+            # add the current value to the list
+            list_buffer_contents.append(current.value)
+            # update current to the next item in the DLL
+            current = current.next
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
